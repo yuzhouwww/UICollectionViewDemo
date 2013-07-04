5c760ef75447ba862da5c5673da80489f1f8f65b
yuzhouwww <469258988@qq.com>
2013-07-05 00:16:09 +0800

    Initial Commit

---
+++ CollectionViewTest/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  CollectionViewTest
+//
+//  Created by yuzhou on 13-7-5.
+//  Copyright (c) 2013年 wzyk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ CollectionViewTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CollectionViewTest
+//
+//  Created by yuzhou on 13-7-5.
+//  Copyright (c) 2013年 wzyk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
